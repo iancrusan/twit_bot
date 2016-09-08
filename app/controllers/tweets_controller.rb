3946a941851f5c1c0f309b50011a27442211ddf0
ian crusan <iancrusan@hotmail.com>
2016-09-08 11:52:27 -0400

    made new tweet view

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,17 @@
+class TweetsController < ApplicationController
+  before_action :set_tweet, only: [:show, :edit, :update, :destroy]
+
+  before_filter :authenticate_user!
+
+  def create
+    @tweet = Tweet.new(tweet_params)
+
+    respond_to do |format|
+      if @tweet.save
+        format.html { redirect_to @tweet, notice: 'Tweet was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+end
