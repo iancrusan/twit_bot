dcebdf7488c5e38381cd271d46bee1b8e9a28cb6
ian crusan <iancrusan@hotmail.com>
2016-09-08 17:28:36 -0400

    hung on undefined method username

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,18 +1,19 @@
 class TweetsController < ApplicationController
   before_action :set_tweet, only: [:show, :edit, :update, :destroy]
-
   before_filter :authenticate_user!
+
+  def index
+    @t = Tweet.all
+  end
+
+  def show
+  end
 
   def new
     @tweet = Tweet.new
   end
 
-  def show
-
-  end
-
-  def index
-    @t = Tweet.all
+  def edit
   end
 
   def create
@@ -25,5 +26,32 @@ class TweetsController < ApplicationController
         format.html { render :new }
       end
     end
+  end
+
+  def update
+    respond_to do |format|
+      if @tweet.update(tweet_params)
+        format.html { redirect_to @tweet, notice: 'Post was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+  def destroy
+    @tweet.destroy
+    respond_to do |format|
+      format.html { redirect_to tweets_url, notice: 'Tweet was successfully destroyed.' }
+    end
+  end
+
+  private
+
+  def set_tweet
+    @tweet = Tweet.find(params[:id])
+  end
+
+  def tweet_params
+    params.require(:tweet).permit(:message, :user_id)
   end
 end
